24f9eea21c5c31933e2fcce33f7c1617c71aa611
zibertszn <alepon1337@mail.ru>
2026-03-16 16:56:17 +0300

    init

---
+++ lib/VotingWizard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "VotingWizard/version"
+
+module VotingWizard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/VotingWizard_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe VotingWizard do
+  it "has a version number" do
+    expect(VotingWizard::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
