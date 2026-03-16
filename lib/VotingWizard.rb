c1110aeb7329bcba499677979c071d97a2a765fe
ansavaa <ramrost666@gmail.com>
2026-03-16 17:10:21 +0300

    Добавлена структура библиотеки

--- lib/VotingWizard.rb
+++ lib/VotingWizard.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "VotingWizard/version"
+require_relative "VotingWizard/error"
+require_relative "VotingWizard/option"
+require_relative "VotingWizard/vote"
+require_relative "VotingWizard/poll"
 
 module VotingWizard
   class Error < StandardError; end
