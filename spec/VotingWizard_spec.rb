fc664f284f2a1695e2a62cb460d0f1ea0a913379
ansavaa <ramrost666@gmail.com>
2026-03-16 17:37:49 +0300

    Добавлен RSpec и CI

--- spec/VotingWizard_spec.rb
+++ spec/VotingWizard_spec.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
+require "VotingWizard"
+
 RSpec.describe VotingWizard do
   it "has a version number" do
-    expect(VotingWizard::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(VotingWizard::VERSION).not_to be_nil
   end
 end
